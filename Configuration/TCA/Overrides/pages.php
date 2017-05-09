878a58ff89b9f40efcb26d9d77bf3369dc178169
Riccardo De Contardi <riccardo.decontardi@bhuman.it>
2017-05-09 15:42:23 +0200

    better usage of call_user_func?

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -1,51 +1,55 @@
 <?php
 defined('TYPO3_MODE') or die();
 
-/***************
- * Temporary variables
- */
-$extensionKey = 'bhsiteconf';
+//prevent defining global variables 
+call_user_func(function () {
+    /***************
+     * Temporary variables
+     */
+    $extensionKey = 'bhsiteconf';
+    
+    
+    /* Register pageTs Config files for including at page level */
+    $pageTsFolder = 'Configuration/TypoScript/TSconfig/Page/';
+    
+    $pageTsStorages = [
+        'AdmPanel.ts' 		=> 'Admin Panel',
+        'TCEMAIN.ts' 		=> 'Set Page user and group permissions',
+        '002.TCEFORM.ts' 	=> 'Modify interface',
+    
+        '220.common.content.newtext.ts' => 'New plain text content element',
+        '230.common.content.simpleteaser.ts' => 'New Simple Teaser content element',
+        '240.common.content.videoteaser.ts' => 'New Video Teaser content element',
+        '310.gridelements.twocolumnscontainer.ts' => 'Grid Element, Two columns container',
+      	'320.gridelements.threecolumnscontainer.ts' => 'Grid Element, Three columns container',
+      	'330.gridelements.fourcolumnscontainer.ts' => 'Grid Element, Four columns container',
+      	'350.gridelements.accordion.ts' => 'Grid Element, Accordion',
+      	'360.gridelements.tab.ts' => 'Grid Element, Tabs',
+    
+        '500.RTE.default.ts' => 'Default RTE Configuration',
+        '502.RTE.styles.ts' => 'Allowed styles for RTE links'
+    ];
+    
+    
+    
+    /* Add default TSConfig Typoscript per pages */
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+      '<INCLUDE_TYPOSCRIPT: source="DIR:EXT:'.$extensionKey.'/Configuration/TypoScript/TSconfig/Shared/Pages" extensions="ts">'
+      );
+    
+    
+    /***************
+     * Register PageTS
+     */
+    
+    
+    
+    foreach ($pageTsStorages as $configFile => $name) {
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
+            $extensionKey,
+            $pageTsFolder . $configFile,
+            $name
+        );
+    }
 
-
-/* Register pageTs Config files for including at page level */
-$pageTsFolder = 'Configuration/TypoScript/TSconfig/Page/';
-
-$pageTsStorages = [
-    'AdmPanel.ts' 		=> 'Admin Panel',
-    'TCEMAIN.ts' 		=> 'Set Page user and group permissions',
-    '002.TCEFORM.ts' 	=> 'Modify interface',
-
-    '220.common.content.newtext.ts' => 'New plain text content element',
-    '230.common.content.simpleteaser.ts' => 'New Simple Teaser content element',
-    '240.common.content.videoteaser.ts' => 'New Video Teaser content element',
-    '310.gridelements.twocolumnscontainer.ts' => 'Grid Element, Two columns container',
-  	'320.gridelements.threecolumnscontainer.ts' => 'Grid Element, Three columns container',
-  	'330.gridelements.fourcolumnscontainer.ts' => 'Grid Element, Four columns container',
-  	'350.gridelements.accordion.ts' => 'Grid Element, Accordion',
-  	'360.gridelements.tab.ts' => 'Grid Element, Tabs',
-
-    '500.RTE.default.ts' => 'Default RTE Configuration',
-    '502.RTE.styles.ts' => 'Allowed styles for RTE links'
-];
-
-
-
-/* Add default TSConfig Typoscript per pages */
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-  '<INCLUDE_TYPOSCRIPT: source="DIR:EXT:'.$extensionKey.'/Configuration/TypoScript/TSconfig/Shared/Pages" extensions="ts">'
-  );
-
-
-/***************
- * Register PageTS
- */
-
-
-
-foreach ($pageTsStorages as $configFile => $name) {
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::registerPageTSConfigFile(
-        $extensionKey,
-        $pageTsFolder . $configFile,
-        $name
-    );
-}
+});
--- Configuration/TCA/Overrides/tt_content_element_gridelement.php
+++ Configuration/TCA/Overrides/tt_content_element_gridelement.php
@@ -4,8 +4,8 @@ defined('TYPO3_MODE') or die();
 
 
 //prevent defining global variables 
-call_user_func(function () {
+
 $GLOBALS['TCA']['tt_content']['types']['gridelements_pi1']['columnsOverrides']['media']['config']['maxitems']=1;
-});
+
 
 

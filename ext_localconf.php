903ccca13ed8e174b6d7970f61508cd01868715b
Riccardo De Contardi <erredeco@gmail.com>
2024-12-23 18:30:01 +0100

    [!!!][WIP][TASK] move page tsconfig structure inside the set

--- ext_localconf.php
+++ ext_localconf.php
@@ -6,17 +6,8 @@ defined('TYPO3') or die('Access denied.');
  * Prevents defining global variables
  */
 call_user_func(function () {
-
-	/** 
-	 * Add default TSConfig Typoscript per pages 
-	 */
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-		'<INCLUDE_TYPOSCRIPT: source="DIR:EXT:bhsiteconf/Configuration/TypoScript/TSconfig/Shared/Pages" extensions="typoscript">'
-	);
-
 	/** 
 	 * Configuration for CKEditor 
 	 */
 	$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['myfull'] = 'EXT:bhsiteconf/Configuration/RTE/myfull.yaml';
-
 });

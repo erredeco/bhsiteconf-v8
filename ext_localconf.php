c899a2b59119491fb04f62e15b7fc7492a3da04b
Riccardo De Contardi <erredeco@gmail.com>
2024-12-23 17:48:59 +0100

    [!!!][TASK] Follow-up use v.13 method to load user tsconfig - delete unused files and code

--- ext_localconf.php
+++ ext_localconf.php
@@ -15,17 +15,6 @@ call_user_func(function () {
 	);
 
 	/** 
-	 * Add default TSConfig Typoscript per be usergroups 
-	 */
-	/* 
-		Obsolete in version 13!!
-		See: https://docs.typo3.org/m/typo3/reference-typoscript/main/en-us/UsingSettingTSconfig/UserTSconfig.html#usersettingdefaultusertsconfig
-
-	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig('
-		<INCLUDE_TYPOSCRIPT: source="DIR:EXT:bhsiteconf/Configuration/TypoScript/TSconfig/Shared/Users" extensions="typoscript">'
-	);
-	*/
-	/** 
 	 * Configuration for CKEditor 
 	 */
 	$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['myfull'] = 'EXT:bhsiteconf/Configuration/RTE/myfull.yaml';
